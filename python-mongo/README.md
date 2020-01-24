704f5b16ad2a06f9d0a66300aa3d888de1c4714a
Ale <alejandro.m@vix.com>
2020-01-24 17:44:18 -0300

    python 3.7 with pymongo document insert example

---
+++ python-mongo/README.md
@@ -0,0 +1,8 @@
+
+##Docker Python 3.7 +pymongo
+##mongo 4.2
+
+python 3.7 with pymongo document insert example
+
+requirements.txt and files
+into src directory for testing
---
+++ python-mongo/src/app.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+from pymongo import MongoClient
+import datetime
+import os
+
+mongo_client = MongoClient(
+                os.environ['MONGO_HOST'] + ':' +  os.environ['MONGO_PORT'],
+                username=os.environ['APP_MONGO_USER'],
+                password=os.environ['APP_MONGO_PASS'],
+                authSource=os.environ['APP_MONGO_DB']
+            )
+
+
+import pymongo
+client = pymongo.MongoClient()
+db = mongo_client[ os.environ['APP_MONGO_DB'] ]
+collection = db[ 'test' ]
+print(collection.insert_one( {'foo' : 'bar' }))
