c7ed7aed75621f22875ec565d3d8443750da2a74
Ale <elmarcu@gmail.com>
2024-10-12 12:04:07 -0400

    aws-cli cloudfront cors fixes

--- aws-shell/scripts/cloudfront/cf_create_distribution.sh
+++ aws-shell/scripts/cloudfront/cf_create_distribution.sh
@@ -3,8 +3,6 @@
 DATE=$(date +%s)
 sed -e "s|\${DATE}|$DATE|g" \
 -e "s|\${BUCKET_OUTPUT}|$BUCKET_OUTPUT|g" \
--e "s|\${CLOUDFRONT_ORIGIN_PATH}|$CLOUDFRONT_ORIGIN_PATH|g" \
--e "s|\${CLOUDFRONT_DEFAULT_ROOT_OBJECT}|$CLOUDFRONT_DEFAULT_ROOT_OBJECT|g" \
 /scripts/cloudfront/config.json > /scripts/cloudfront/config-filled.json    
 
 # CloudFront distribution creation
